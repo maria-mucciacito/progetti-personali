447e17aa6a185539a990db085a59ec73aa737c77
mary023010 <mucciacitomaria@gmail.com>
2023-01-14 16:42:00 +0100

    .NET 6 Web API with Entity Framework Core and SQL Server. Angular 13 CRUD App. Full stack.

---
+++ InspectionApi/InspectionApi/InspectionType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionApi
+{
+    public class InspectionType
+    {
+        public int Id { get; set; }
+        
+        [StringLength(20)]
+        public string InspectionName { get; set; } = string.Empty;
+    }
+}
