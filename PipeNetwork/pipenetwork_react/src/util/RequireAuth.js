21fe173ab9c53374fa59f1614416d1ad3ad57226
Maria <96469875+mary023010@users.noreply.github.com>
2023-03-13 12:12:07 +0100

    .

--- PipeNetwork/pipenetwork_react/src/util/RequireAuth.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-export const RequireAuth = () => {
-  const auth = JSON.parse(localStorage.getItem("user"));
-
-  return auth?.accessToken ? <Outlet /> : <Navigate to="/login" />;
-};
